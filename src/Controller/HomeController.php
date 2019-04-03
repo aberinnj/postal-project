40b6612d329aedb167fa9f46e7051077e71b0c59
Norman Aberin <npaberin@gmail.com>
2019-04-03 11:43:46 -0500

    Added new pages; Removed navbar dropdown placeholders; Added order, support and track routes;

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController {
     /**
      * @Route("/", name="app-home")
      */
-    public function display()
+    public function home_display()
     {
         return $this->render('home/home.html.twig');
     }
@@ -19,16 +19,40 @@ class HomeController extends AbstractController {
     /**
      * @Route("/support", name="app-support")
      */
-    public function help()
+    public function support_help()
     {
-        return $this->render('home/home.html.twig');
+        return $this->render('home/support.html.twig');
     }
 
     /**
      * @Route("/login", name="app-login")
      */
-    public function sign()
+    public function sign_in()
     {
         return $this->render('home/login.html.twig');
+    }
+
+    /**
+     * @Route("/order", name="app-ship-order")
+     */
+    public function order()
+    {
+        return $this->render('home/order.html.twig');
+    }
+
+    /**
+     * @Route("/track", name="app-track")
+     */
+    public function track()
+    {
+        return $this->render('home/track.html.twig');
+    }
+
+    /**
+     * @Route("/track/{id}", name="app-track-id")
+     */
+    public function track_id()
+    {
+        return $this->render('home/track-result.html.twig', [ 'trackingID' => id ]);
     }
 }
